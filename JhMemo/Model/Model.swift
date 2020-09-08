0fa438fed755a7603ef961d5f0f746d2c37f3fa2
test <test>
2020-09-08 23:38:19 +0900

    Memo Class

---
+++ JhMemo/Model/Model.swift
@@ -0,0 +1,26 @@
+//
+//  Model.swift
+//  JhMemo
+//
+//  Created by jh on 2020/09/08.
+//  Copyright © 2020 jh. All rights reserved.
+//
+
+import Foundation
+
+class Memo {
+    var content: String //메모
+    var insertDate: Date //날짜
+    
+    //클래스니까 속성 초기화 필수!
+    init(content: String) {
+        self.content = content
+        insertDate = Date()
+    }
+    
+    //범위를 배열에 미리 저장?
+    static var dummyMemoList = [
+        Memo(content: "ㅎㅎ"),
+        Memo(content: "메모 테스트")
+    ]
+}
